f0e0049bc1afa21d726df08cdaba13cc00109574
Joerg Orlowski <joerg.orlowski@bgrci.de>
2021-04-29 06:52:53 +0200

    Initial Import

--- Verzeichnis sortieren/src/de/bgrci/verzeichnis/VerzeichnisSort.java
+++ Verzeichnis sortieren/src/de/bgrci/verzeichnis/VerzeichnisSort.java
@@ -15,6 +15,7 @@ import com.drew.metadata.Metadata;
 import com.drew.metadata.exif.ExifSubIFDDirectory;
 
 public class VerzeichnisSort {
+	
 	public static void main(String[] args) {
 		durchsucheVerzeichns(new File("d:\\tmp\\joerg\\"));
 	}
@@ -28,34 +29,34 @@ public class VerzeichnisSort {
 			if (f.isDirectory()) durchsucheVerzeichns(f);
 		 else {
 				if (f.getName().toLowerCase().endsWith(".jpg")) speicherBild(f);
-				
 			}
 		}
 	}
 	
 	private static void speicherBild(File bild) {
+		Date date = null;
 		try {
 			System.out.println("\nBild " + bild.getName());
 			Metadata metadata = JpegMetadataReader.readMetadata(bild);
-
 			ExifSubIFDDirectory directory = metadata.getFirstDirectoryOfType(ExifSubIFDDirectory.class);
 
-			Date date = directory.getDate(ExifSubIFDDirectory.TAG_DATETIME_ORIGINAL);
+			date = directory.getDate(ExifSubIFDDirectory.TAG_DATETIME_ORIGINAL);
 			
 			System.out.println("Datum : " + date);
 			
 			System.out.println("Pfad " + getPfad(date));
+		}
+		catch (Exception e) {
+			System.out.println("Keine EXIF Daten");
+			date = null;
+		}
 			
-			try {
-				Files.createDirectories(Path.of(new File("d:\\tmp\\" + getPfad(date)).toURI()));
-				Files.copy(Path.of(bild.getAbsoluteFile().toURI()), Path.of(   (new File("d:\\tmp\\" + getPfad(date) + bild.getName())).toURI()   ) , java.nio.file.StandardCopyOption.REPLACE_EXISTING );
-			} catch (Exception a) {
-				System.out.println("Kopieren fehlgeschlagen! " + bild.getName());
-				a.printStackTrace();
-			}
-		} catch (ImageProcessingException | IOException e) {
-			// TODO Auto-generated catch block
-			System.out.println("Keine EXIF Daten.");
+		try {
+			Files.createDirectories(Path.of(new File("d:\\tmp\\" + getPfad(date)).toURI()));
+			Files.copy(Path.of(bild.getAbsoluteFile().toURI()), Path.of(   (new File("d:\\tmp\\" + getPfad(date) + bild.getName())).toURI()   ) , java.nio.file.StandardCopyOption.REPLACE_EXISTING );
+		} catch (Exception a) {
+			System.out.println("Kopieren fehlgeschlagen! " + bild.getName());
+			a.printStackTrace();
 		}
 
 	}
@@ -63,7 +64,11 @@ public class VerzeichnisSort {
 	private static String getPfad(Date date) {
 		String pfad = "";
 		
-		pfad = (date.getYear() + 1900) + "\\" + (date.getMonth() + 1) + "\\";
+		if (date == null) {
+			pfad = "keine_EXIF_Daten\\";
+		} else {
+			pfad = (date.getYear() + 1900) + "\\" + (date.getMonth() + 1) + "\\";
+		}
 		
 		return pfad;
 	}
